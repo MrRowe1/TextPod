094de3c2f4958c0e2371371c8b7271ef29f49679
luojiao <luojiao@xiaoluozhuanshu.local>
2019-12-13 09:35:06 +0800

    测试封装cocoapods第三方库

---
+++ cocoaText/cocoaText/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cocoaText
+//
+//  Created by luojiao on 2019/12/13.
+//  Copyright © 2019 luojiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ cocoaText/cocoaText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocoaText
+//
+//  Created by luojiao on 2019/12/13.
+//  Copyright © 2019 luojiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
